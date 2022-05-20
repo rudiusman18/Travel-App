a5441c7d8b5981e624a7355b0e697be51cfb1fba
rudi-usman <commander.heaven@gmail.com>
2022-05-20 16:31:36 +0700

    Aplikasi Travel

---
+++ lib/shared/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = Color(0xff5C40CC);
+Color secondaryColor = Color(0xff998EFD);
+Color titleColor = Color(0xff1F1449);
+Color backgroundColor = Color(0xffDBD7EC);
+Color subtitleColor = Color(0xff9698A9);
+Color whiteColor = Color(0xffFFFFFF);
+
+// ! MANDI DULU NANTI DILANJUT
