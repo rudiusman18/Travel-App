169374a92eb816a0a457a298a5880b026864f9db
rudi-usman <commander.heaven@gmail.com>
2022-05-20 17:05:23 +0700

    Update 20 Mei 2022 17:05

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -7,5 +7,32 @@ Color titleColor = Color(0xff1F1449);
 Color backgroundColor = Color(0xffDBD7EC);
 Color subtitleColor = Color(0xff9698A9);
 Color whiteColor = Color(0xffFFFFFF);
+Color greenColor = Color(0xff0EC3AE);
+Color redColor = Color(0xffEB70A5);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: primaryColor,
+);
+
+TextStyle secondaryTextStyle = GoogleFonts.poppins(
+  color: secondaryColor,
+);
+
+TextStyle titleTextStyle = GoogleFonts.poppins(
+  color: titleColor,
+);
+
+TextStyle backgroundTextStyle = GoogleFonts.poppins(
+  color: backgroundColor,
+);
+
+TextStyle subtitleTextStyle = GoogleFonts.poppins(
+  color: subtitleColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+);
+
 
 // ! MANDI DULU NANTI DILANJUT
